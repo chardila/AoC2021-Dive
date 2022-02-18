67cd9877de21132bdde77058fc7c932e9edb6d0d
Carlos Humberto Ardila Arango <carlos@cardila.com>
2022-02-18 15:10:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    INPUT_FILE = "inputs/02_Dive.txt"
+    with open(INPUT_FILE) as f:
+        input = [{x.split(" ")[0]: int(x.split(" ")[1])} for x in f.read().split("\n")[:-1]]
 
-    INPUT_FILE = "inputs/01_sonar_sweep.txt"
+    # Both stars
+    depth, pos, aim, depth2 = 0, 0, 0, 0
+    for i in input:
+        for command, value in i.items():
+            if command == "forward":
+                pos += value
+                depth2 += aim * value
+            elif command == "up":
+                depth -= value
+                aim -= value
+            elif command == "down":
+                depth += value
+                aim += value
 
-    with open(INPUT_FILE, "r") as f:
-        depths = [int(line) for line in f]
-
-    count = 0
-    for i in range(len(depths) - 1):
-        if depths[i] < depths[i + 1]:
-            count += 1
-
-    print("Sonar Sweep - Part 1:", count)
-
-    count2 = 0
-    for i in range(3, len(depths)):
-        left = depths[i - 3] + depths[i - 2] + depths[i - 1]
-        right = depths[i - 2] + depths[i - 1] + depths[i]
-        if left < right:
-            count2 += 1
-
-    print("Sonar Sweep - Part 2:", count2)
+    print("Dive Part 1:", depth * pos)
+    print("Dive Part 2:", depth2 * pos)
